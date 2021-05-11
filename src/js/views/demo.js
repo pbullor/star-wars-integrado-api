f967353537cc7c05c20fa3a90d6c5d5f4007545f
pbullor <pablobullor@gmail.com>
2021-05-11 13:53:48 +0000

    Agrego Cards

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,40 +1,23 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
 import { Context } from "../store/appContext";
-
 import "../../styles/demo.scss";
+import Card from "../component/card";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
+			<div className="row d-flex-row flex-nowrap overflow-auto mt-3">
+				{store.personajes.map((item, index) => {
 					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
+						<div className="col-lg-4 mb-5" key={index}>
+							<Card />
+						</div>
 					);
 				})}
-			</ul>
-			<br />
+			</div>
 			<Link to="/">
 				<button className="btn btn-primary">Back home</button>
 			</Link>
